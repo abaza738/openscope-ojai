7bb5b3ce296b184a8947ddda6b52ac9508366445
Maher Abaza <abaza.maher.96@gmail.com>
2020-10-06 23:08:52 +0300

    Updated JSON, gitignore

--- convert.py
+++ convert.py
@@ -1,13 +1,29 @@
 import os
-myfile = open("Desktop\\fixes.txt", "r")
-dest = open("Desktop\\fixed_fixes.txt", "w")
+myfile = open(".\TWR.txt", "r")
+dest = open(".\TWR_CONV.txt", "w")
 for line in myfile:
-    splits = line.split(" ")
-    fix_name = splits[0]
-    nd = splits[1]
-    nm = splits[2]
-    ns = splits[3]+"."+splits[4]
-    ed = splits[5]
-    em = splits[6]
-    es = splits[7]+"."+splits[8].split("\n")[0]
-    dest.write("\""+fix_name+"\""+": [\""+nd+"d"+nm+"m"+ns+"\", \""+ed+"d"+em+"m"+es+"\"],"+"\n")
+    coord = line.split(" ")
+    
+    north = coord[0].split(".")
+    nd=north[0]
+    nm=north[1]
+    ns=north[2]
+    
+    east = coord[1].split(".")
+    ed=east[0]
+    em=east[1]
+    es=east[2]
+    
+    north1 = coord[2].split(".")
+    nd1=north1[0]
+    nm1=north1[1]
+    ns1=north1[2]
+
+    east1 = coord[3].split(".")
+    ed1=east1[0]
+    em1=east1[1]
+    es1=east1[2]
+
+    dest.write("[\""+nd+"d"+nm+"m"+ns+"\", \""+ed+"d"+em+"m"+es+"\", \""+nd1+"d"+nm1+"m"+ns1+"\", \""+ed1+"d"+em1+"m"+es1+"\"],\n")
+
+print("Done!")
